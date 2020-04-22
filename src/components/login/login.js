5b2cc63996c3da267e29b2fbae465b83aaef68ef
Zhar.M <mehdizhar344@gmail.com>
2020-04-22 02:32:39 +0000

    Register and Login components

---
+++ src/components/login/login.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import loginImg from '../../img/login.svg';
+import './style.scss';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+  render() {
+    return (
+      <div className='auth-container'>
+        <div className='base-container' ref={this.props.containerRef}>
+          <div className='header'>Login</div>
+          <div className='content'>
+            <div className='image'>
+              <img src={loginImg} />
+            </div>
+            <div className='form'>
+              <div className='form-group'>
+                <label htmlFor='username'>Username</label>
+                <input
+                  type='text'
+                  name='username'
+                  placeholder='username'
+                  value={this.state.username}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='password'>Password</label>
+                <input
+                  type='password'
+                  name='password'
+                  placeholder='password'
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='footer'>
+            <button type='button' className='btn btn-primary'>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/login/register.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import loginImg from '../../img/login.svg';
+import './style.scss';
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='auth-container'>
+        <div className='base-container' ref={this.props.containerRef}>
+          <div className='header'>Register</div>
+          <div className='content'>
+            <div className='image'>
+              <img src={loginImg} />
+            </div>
+            <div className='form'>
+              <div className='form-group'>
+                <label htmlFor='username'>Username</label>
+                <input type='text' name='username' placeholder='username' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='email'>Email</label>
+                <input type='text' name='email' placeholder='email' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='password'>Password</label>
+                <input type='text' name='password' placeholder='password' />
+              </div>
+            </div>
+          </div>
+
+          <button type='button' className='btn btn-primary'>
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Register;

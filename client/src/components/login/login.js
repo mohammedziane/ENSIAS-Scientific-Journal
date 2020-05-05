87f5ad7a33acc7a938546efe21380e7aa19dcb25
Othmane-zarai <57667825+Othmane-zarai@users.noreply.github.com>
2020-05-05 00:16:48 +0000

    Integration With Back End
    Using JWT (JSON Web Token)

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,16 +1,77 @@
 import React from 'react';
 import loginImg from '../../img/login.svg';
 import './style.scss';
+import AuthService from "../../JWT-Services/auth.service";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import { Redirect } from 'react-router-dom';
+const required = value => {
+    if (!value) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This field is required!
+            </div>
+        );
+    }
+};
 
 export default class Login extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
+      super(props);
+      this.handleLogin = this.handleLogin.bind(this);
+      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangePassword = this.onChangePassword.bind(this);
+      this.state = {
+          username: '',
+          password: '',
+          loading: false,
+          message: "",
+          redirect: false
+      };
   }
-  render() {
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+    handleLogin(e) {
+        e.preventDefault();
+        this.setState({
+            message: "",
+            loading: true
+        });
+
+
+            AuthService.login(this.state.username, this.state.password).then(
+                () => {
+                    console.log("Login Success");
+                    this.setState({
+                        redirect: true
+                    })
+                    window.location.reload();
+                },
+                error => {
+                    const resMessage = (error.response &&
+                        error.response.data && error.response.data.message)
+                        || error.message || error.toString();
+                    this.setState({
+                        loading: false,
+                        message: resMessage
+                    });
+                }
+            );
+
+    }
+    render() {
+        const { redirect } = this.state;
+        if (redirect) {
+            return <Redirect to='/dashbord' />;
+        }
     return (
       <div className='auth-container'>
         <div className='base-container' ref={this.props.containerRef}>
@@ -18,34 +79,41 @@ export default class Login extends React.Component {
           <div className='content'>
             <div className='image'>
               <img src={loginImg} />
-            </div>
-            <div className='forme'>
-              <div className='forme-groupe'>
+                    </div>
+                    <Form
+                        onSubmit={this.handleLogin}
+                        >
+            <div className='form'>
+              <div className='form-group'>
                 <label htmlFor='username'>Username</label>
-                <input
-                  type='text'
-                  name='username'
-                  placeholder='username'
-                  value={this.state.username}
-                  onChange={this.onChange}
+                <Input
+                                    type='text'
+                                    name='username'
+                                    placeholder='username'
+                                    value={this.state.username}
+                                    onChange={this.onChangeUsername}
+                                    validations={[required]}
                 />
               </div>
-              <div className='forme-groupe'>
+              <div className='form-group'>
                 <label htmlFor='password'>Password</label>
-                <input
-                  type='password'
-                  name='password'
-                  placeholder='password'
-                  value={this.state.password}
-                  onChange={this.onChange}
+                <Input
+                                    type='password'
+                                    name='password'
+                                    placeholder='password'
+                                    value={this.state.password}
+                                    onChange={this.onChangePassword}
+                                    validations={[required]}
                 />
-              </div>
-            </div>
-          </div>
-          <div className='footer'>
-            <button type='button' className='btn btn-primary'>
-              Login
-            </button>
+                            </div>
+                            <div className='footer'>
+                                <button type='submit' className='btn btn-primary'>
+                                    Login
+                                </button>
+                            </div>
+
+                        </div>
+                        </Form>
           </div>
         </div>
       </div>
--- client/src/components/login/register.js
+++ client/src/components/login/register.js
@@ -1,11 +1,92 @@
 import React from 'react';
 import loginImg from '../../img/login.svg';
 import './style.scss';
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import { isEmail } from "validator";
+import AuthService from "../../JWT-Services/auth.service";
+const required = value => {
+    if (!value) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This field is required!
+            </div>
+        );
+    }
+};
+const email = value => {
+    if (!isEmail(value)) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This is not a valid email.
+            </div>
+        );
+    }
+};
 class Register extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+      super(props);
+      this.handleRegister = this.handleRegister.bind.this;
+      this.onChangeUsername = this.onChangeUsername.bind.this;
+      this.onChangeEmail = this.onChangeEmail.bind.this;
+      this.onChangePassword = this.onChangePassword.bind.this;
+      this.state = {
+          username: "",
+          email: "",
+          password: "",
+          successful: false,
+          message:""
+      };
   }
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+    handleRegister(e) {
+        console.log("Bdya dyal Handle Register!");
+        e.preventDefault();
+        this.setState({
+            message: "",
+            successful: false
+        });
+        console.log("Rani hna ! ");
+        AuthService.register(
+            this.state.username,
+            this.state.password,
+            this.state.email
+        ).then(
+            response => {
+                this.setState({
+                    message: response.data.message,
+                    successful: true
+                });
+                console.log("User Registred !");
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message
+                    ) || error.message
+                    || error.toString();
+                this.setState({
+                    successful: false,
+                    message: resMessage
+                });
+            }
+        );
+    }
 
   render() {
     return (
@@ -15,26 +96,41 @@ class Register extends React.Component {
           <div className='content'>
             <div className='image'>
               <img src={loginImg} />
-            </div>
-            <div className='forme'>
-              <div className='forme-groupe'>
+                    </div>
+                    <Form
+                        onSubmit={this.handleRegister}>
+            <div className='form'>
+              <div className='form-group'>
                 <label htmlFor='username'>Username</label>
-                <input type='text' name='username' placeholder='username' />
+                                <Input type='text' name='username' placeholder='username'
+                                    value={this.state.username}
+                                    onChange={this.onChangeUsername}
+                                    validations={[required]}
+                                />
               </div>
-              <div className='forme-groupe'>
+              <div className='form-group'>
                 <label htmlFor='email'>Email</label>
-                <input type='text' name='email' placeholder='email' />
+                                <Input type='text' name='email' placeholder='email'
+                                    value={this.state.email}
+                                    onChange={this.onChangeEmail}
+                                    validations={[required,email]}
+                                />
               </div>
-              <div className='forme-groupe'>
+              <div className='form-group'>
                 <label htmlFor='password'>Password</label>
-                <input type='text' name='password' placeholder='password' />
-              </div>
-            </div>
-          </div>
+                                <Input type='password' name='password' placeholder='password'
+                                    value={this.state.password}
+                                    onChange={this.onChangePassword}
+                                    validations={[required]}
+                                />
 
-          <button type='button' className='btn btn-primary'>
-            Register
-          </button>
+                            </div>
+                            <button type='submit' className='btn btn-primary'>
+                                Register
+                                </button>
+                        </div>
+                    </Form>
+          </div>
         </div>
       </div>
     );

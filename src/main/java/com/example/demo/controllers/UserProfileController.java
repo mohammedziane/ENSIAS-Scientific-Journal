cc9e57c648bb4ac3427d3f5c6f071a30f0253ec9
Zhar.M <mehdizhar344@gmail.com>
2020-06-16 23:42:43 +0100

    Create Profile Controller

---
+++ src/main/java/com/example/demo/controllers/UserProfileController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controllers;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.models.*;
+import com.example.demo.repository.Profile_Repository;
+import com.example.demo.repository.User_Repository;
+import com.example.demo.security.CurrentUser;
+import com.example.demo.security.UserPrincipal;
+import com.example.demp.REQ_RES.ApiResponse;
+import com.example.demp.REQ_RES.JwtAuthenticationResponse;
+import com.example.demp.REQ_RES.LoginRequest;
+import com.example.demp.REQ_RES.UserSummary;
+import com.example.demp.REQ_RES.ProfileSummary;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class UserProfileController {
+	
+	
+	@Autowired
+	Profile_Repository profileRepository;
+	@Autowired
+	User_Repository userRepository;
+	@GetMapping("/user/{username}")
+	public ResponseEntity<?> getProfile(@PathVariable("username") String username){
+		   User user= userRepository.findByUsername(username);
+		   Profile profile = profileRepository.findByUser(user);
+			ProfileSummary profileSummary = new ProfileSummary(profile.getIdProfile(),profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),profile.getSkills(),profile.getGithub(),profile.getBio(),profile.getDate(),profile.getUpdatedAt());
+			return ResponseEntity.ok(profileSummary);
+	}
+	@PostMapping("/profile/{username}")
+	public ResponseEntity<?> setProfile(@PathVariable("username") String username ,@RequestBody ProfileSummary profileSummaryRequest){
+		User user= userRepository.findByUsername(username);   
+		Profile profile = profileRepository.findByUser(user);
+	    profile.setBio(profileSummaryRequest.getBio());
+	    profile.setGithub(profileSummaryRequest.getGithub());
+	    profile.setCompany(profileSummaryRequest.getCompany());
+	    profile.setGender(profileSummaryRequest.getGender());
+	    profile.setLocation(profileSummaryRequest.getLocation());
+	    profile.setSkills(profileSummaryRequest.getSkills());
+	    profile.setStatus(profileSummaryRequest.getStatus());
+		profile.setWebsite(profileSummaryRequest.getWebsite());
+	    Profile profileUpdated = profileRepository.save(profile);
+		ProfileSummary profileSummary = new ProfileSummary(profileUpdated.getIdProfile(),profileUpdated.getGender(),profileUpdated.getCompany(),profileUpdated.getWebsite(),profileUpdated.getLocation(),profileUpdated.getStatus(),profileUpdated.getSkills(),profileUpdated.getGithub(),profileUpdated.getBio(),profileUpdated.getDate(),profileUpdated.getUpdatedAt());
+		return ResponseEntity.ok(profileSummary);   
+	}
+	
+	
+	
+
+}

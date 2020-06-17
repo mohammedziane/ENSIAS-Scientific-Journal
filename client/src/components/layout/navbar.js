3e7baeae2e2f54b7a581043f14ed810b83bb643c
Zhar.M <mehdizhar344@gmail.com>
2020-06-17 02:53:45 +0100

    Connect navbar with redux

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import logout from '../../actions-services/logout';
 
-const navbar = () => {
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i> onScience
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  authLinks = (
+    <ul>
+      <li>
+        <Link to='/dashboard/profiles' onClick={this.props.logout}>
+          Scientists
         </Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to='/profiles'>Scientists</Link>
-        </li>
-        <li>
-          <Link to='/auth'>Sign In || Sign Up</Link>
-        </li>
-      </ul>
-    </nav>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
+        |
+        <Link to='/dashboard' title='Dashboard'>
+          <i className='fas fa-user'></i>
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <Link to='/' title='Logout' onClick={this.props.logout}>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </Link>
+      </li>
+    </ul>
   );
-};
+  guestLinks = (
+    <ul>
+      <li>
+        <Link to='/profiles'>Scientists</Link>
+      </li>
+      <li>
+        <Link to='/auth'>Sign In || Sign Up</Link>
+      </li>
+    </ul>
+  );
+  render() {
+    return (
+      <nav className='navbar bg-dark'>
+        <h1>
+          <Link to='/'>
+            <i className='fas fa-code'></i> onScience
+          </Link>
+        </h1>
+        {
+          <div>
+            {this.props.isAuthenticated ? this.authLinks : this.guestLinks}
+          </div>
+        }
+      </nav>
+    );
+  }
+}
 
-export default navbar;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  loading: state.auth.loading,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);

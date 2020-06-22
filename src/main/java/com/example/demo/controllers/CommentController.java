ce2b916598eedae78f272b6cd8dcb9ee5c8f581a
Zhar.M <mehdizhar344@gmail.com>
2020-06-22 07:25:01 +0100

    Getmapping comments / profiles

--- src/main/java/com/example/demo/controllers/CommentController.java
+++ src/main/java/com/example/demo/controllers/CommentController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,7 @@ import com.example.demo.models.*;
 import com.example.demo.repository.Comment_Repository;
 import com.example.demo.repository.Poste_Repository;
 import com.example.demo.repository.User_Repository;
+import com.example.demp.REQ_RES.ApiResponse;
 import com.example.demp.REQ_RES.CommentSummary;
 
 @RestController
@@ -29,8 +33,23 @@ public class CommentController {
 		User user = userRepository.findByUsername(username);
 		Poste poste = posteRepository.findPosteById(id_poste);
 		Comment comment = new Comment(commentSummary.getName(),commentSummary.getText(),user,poste,commentSummary.getDate());
-		commentRepository.save(comment);
-		CommentSummary comment_added = new CommentSummary(commentSummary.getName(),commentSummary.getText(),commentSummary.getDate());
-		return ResponseEntity.ok(comment_added);
+		Comment result = commentRepository.save(comment);
+		CommentSummary envoye = new CommentSummary(result.getIdCommen(),id_poste,username,result.getText(),result.getDate());
+		return ResponseEntity.ok(envoye);
+	}
+	@GetMapping("/postes/getposte/{id_poste}")
+	public List<Long> getAllComments(@PathVariable("id_poste") Long id_poste){
+		Poste poste = posteRepository.getOne(id_poste);
+		return commentRepository.findCommentsByPoste(poste);
+	}
+	@GetMapping("/comments/{id}")
+	public ResponseEntity<?> getComment(@PathVariable("id") Long id){
+		if(commentRepository.existsById(id)) {
+		Comment comment = commentRepository.findByCommentId(id);
+		Poste poste = comment.getPoste();
+		CommentSummary envoye = new CommentSummary(comment.getIdCommen(),poste.getId_poste(),comment.getName(),comment.getText(),comment.getDate());
+		return ResponseEntity.ok(envoye);
+		}
+		return ResponseEntity.ok("Aucun commentaires pour ce poste! Commenter...");
 	}
 }
--- src/main/java/com/example/demo/controllers/UserProfileController.java
+++ src/main/java/com/example/demo/controllers/UserProfileController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.*;
+import com.example.demo.repository.Education_Repository;
+import com.example.demo.repository.Experience_Repository;
 import com.example.demo.repository.Profile_Repository;
 import com.example.demo.repository.User_Repository;
 import com.example.demo.security.CurrentUser;
@@ -23,6 +26,7 @@ import com.example.demo.security.UserPrincipal;
 import com.example.demp.REQ_RES.ApiResponse;
 import com.example.demp.REQ_RES.JwtAuthenticationResponse;
 import com.example.demp.REQ_RES.LoginRequest;
+import com.example.demp.REQ_RES.ProfileData;
 import com.example.demp.REQ_RES.UserSummary;
 import com.example.demp.REQ_RES.ProfileSummary;
 
@@ -31,7 +35,10 @@ import com.example.demp.REQ_RES.ProfileSummary;
 @RestController
 @RequestMapping("/api")
 public class UserProfileController {
-		
+	@Autowired
+	private Experience_Repository experienceRepository;
+	@Autowired 
+	private Education_Repository educationRepository;
 	@Autowired
 	Profile_Repository profileRepository;
 	@Autowired
@@ -41,7 +48,7 @@ public class UserProfileController {
 		User user = userRepository.findByUsername(username);
 		if(profileRepository.existsByUser(user)) {
             Profile profile = profileRepository.findByUser(user);
-            ProfileSummary profileSummary = new ProfileSummary(profile.getIdProfile(),profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),profile.getSkills(),profile.getGithub(),profile.getBio(),profile.getDate(),profile.getUpdatedAt());
+            ProfileSummary profileSummary = new ProfileSummary(profile.getIdProfile(),user.getId_user(),user.getUsername(),profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),profile.getSkills(),profile.getGithub(),profile.getBio(),profile.getDate(),profile.getUpdatedAt());
             return ResponseEntity.ok(profileSummary);
         }
         return ResponseEntity.ok(new ApiResponse(false,"Vous n'avez pas encore créer votre profile"));
@@ -51,7 +58,7 @@ public class UserProfileController {
 		User user= userRepository.findByUsername(username);   
 		Profile profile = new Profile(user,profileSummaryRequest.getGender(),profileSummaryRequest.getCompany(),profileSummaryRequest.getWebsite(),profileSummaryRequest.getLocation(),profileSummaryRequest.getStatus(),profileSummaryRequest.getSkills(),profileSummaryRequest.getGithub(),profileSummaryRequest.getBio());
 	    Profile profileUpdated = profileRepository.save(profile);
-		ProfileSummary profileSummary = new ProfileSummary(profileUpdated.getIdProfile(),profileUpdated.getGender(),profileUpdated.getCompany(),profileUpdated.getWebsite(),profileUpdated.getLocation(),profileUpdated.getStatus(),profileUpdated.getSkills(),profileUpdated.getGithub(),profileUpdated.getBio(),profileUpdated.getDate(),profileUpdated.getUpdatedAt());
+		ProfileSummary profileSummary = new ProfileSummary(profileUpdated.getIdProfile(),user.getId_user(),user.getUsername(),profileUpdated.getGender(),profileUpdated.getCompany(),profileUpdated.getWebsite(),profileUpdated.getLocation(),profileUpdated.getStatus(),profileUpdated.getSkills(),profileUpdated.getGithub(),profileUpdated.getBio(),profileUpdated.getDate(),profileUpdated.getUpdatedAt());
 		return ResponseEntity.ok(profileSummary);   
 	}
 	
@@ -62,8 +69,19 @@ public class UserProfileController {
 	@GetMapping("/profiles/showprofile/{id_profile}")
 	public ResponseEntity<?> getProfileById(@PathVariable("id_profile") Long id_profile){
 		 Profile profile = profileRepository.findByIdProfile(id_profile);
-			ProfileSummary profileSummary = new ProfileSummary(profile.getIdProfile(),profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),profile.getSkills(),profile.getGithub(),profile.getBio(),profile.getDate(),profile.getUpdatedAt());
-		 return ResponseEntity.ok(profile);
+		 User user = profile.getUser();
+			ProfileSummary profileSummary = new ProfileSummary(profile.getIdProfile(),user.getId_user(),user.getUsername(),profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),profile.getSkills(),profile.getGithub(),profile.getBio(),profile.getDate(),profile.getUpdatedAt());
+		 return ResponseEntity.ok(profileSummary);
+		}
+	
+	@GetMapping("/profiles/selected/{id_user}")
+	public ResponseEntity<?> getProfileByUser(@PathVariable("id_user") Long id_user){
+		User user = userRepository.getOne(id_user);
+		List<String> educations = educationRepository.SelectEducationsById(user);
+		ArrayList<String> id_experiences = experienceRepository.SelectExperiencessById(user);
+		 Profile profile = profileRepository.findByUser(user);
+			ProfileData profileData = new ProfileData(profile.getIdProfile(),id_user,profile.getGender(),profile.getCompany(),profile.getWebsite(),profile.getLocation(),profile.getStatus(),user.getUsername(),profile.getSkills(),profile.getBio(),educations,id_experiences);
+		 return ResponseEntity.ok(profileData);
 		}
 }
 	
